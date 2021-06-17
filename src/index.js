2b77e148f603151916804ec29686848118101372
Eder <chlrobert93@gmail.com>
2021-06-17 12:59:33 -0500

    Inicialización de state a través de contructor

--- src/index.js
+++ src/index.js
@@ -2,19 +2,59 @@ import React from 'react';
 import ReactDom from 'react-dom';
 //import seasonsDisplay from 'seasonsDisplay';
 
-
+//Estamos creando un clase nueva dentro de JS
+//Que solo tiene un método asignado
+/*
 const App = () => {
-      //Obtner geolocalización
-      navigator.geolocation.getCurrentPosition(
+      Window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+      );
+}*/
+
+
+
+class App extends React.Component{
+
+      //L afunción contructora es la primera función que se llamará cada vez que se cree una instancia de esta vlase
+      //Cada vez que creamos una nieva instancia del componente de aplicación y la mostramos en la pantalla
+      //Esta función se llamará automática e instantáneamente antes que cualquier otra cosa
+      //Para inicializar el state  de un componente
+
+      constructor (props) {
+            //Se nos exige
+            //Es una referencia a la función contructura del padre 
+            super(props);
+
+
+            //El objecto state
+            //null por que no sabemos que latitud es haora mismo
+            this.state = {lat: null}
+
+      }
+
+
+
+      //Método de renderizado
       
-      //Devolución en parametros
-      position => console.log(position),
-      error => console.log(error)
-  );
-   return <div>Hi there!</div>
-  
-            
-};
+      //React tenemos que definir el render si no definimos esta función aqui
+      //React va a lanzar un error  y decir que no tiene un métodorenderizado que esté devolviendo cualquier jsx
+      render(){
+
+           //Obtner geolocalización
+            window.navigator.geolocation.getCurrentPosition(
+      
+            //Devolución en parametros
+            position => console.log(position),
+            error => console.log(error)
+        );
+            //Devolver
+            return <div>Latitud: </div>
+
+      } 
+
+     
+}
 
 
 ReactDom.render(<App />, document.querySelector('#root'));
