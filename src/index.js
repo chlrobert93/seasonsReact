0f72641472894c043a0caba6c62f331f07d91646
Eder <chlrobert93@gmail.com>
2021-06-24 13:54:42 -0500

    Corregir error CORS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
+import "semantic-ui-css/semantic.min.css";
 
 
 class App extends React.Component{
